9d952391254ec608d82fe54df7b4ac9bda931a02
Adam Stcklov <adamkutak@gmail.com>
2025-02-26 14:10:19 -0500

    5k samples for fid analysis

--- analyze_fid_components.py
+++ analyze_fid_components.py
@@ -90,7 +90,7 @@ def main():
         sampler=sampler,
         device=device,
         branch_keep_configs=[(1, 1), (4, 2), (8, 2), (16, 4), (32, 8)],
-        n_samples=1000,  # Use fewer samples for faster evaluation
+        n_samples=5000,  # Use fewer samples for faster evaluation
     )
 
 
