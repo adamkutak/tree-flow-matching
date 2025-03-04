ada77abff4d5b144ac93a44b8cf18b3d1c8efb17
Adam Stcklov <adamkutak@gmail.com>
2025-03-04 13:06:33 -0500

    test

--- analyze_value_model.py
+++ analyze_value_model.py
@@ -556,6 +556,7 @@ def main():
         flow_model="large_flow_model.pt",
         value_model=None,
         num_channels=256,
+        inception_layer=1,
     )
     # # Run value model prediction analysis
     analysis_results = analyze_value_model_predictions(
