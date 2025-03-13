64b35599f258323c268f44941515a09f7b28d2a7
Adam Stcklov <adamkutak@gmail.com>
2025-03-12 23:43:46 -0400

    test

--- compute_cifar_stats.py
+++ compute_cifar_stats.py
@@ -53,6 +53,7 @@ def compute_cifar10_statistics(feature_dim=64, pca_dim=None):
     transform = transforms.Compose(
         [
             transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
     )
 
