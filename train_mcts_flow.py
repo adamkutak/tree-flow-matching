70a669873247d66d3f98adc412ec71464fcde0d0
Adam Stcklov <adamkutak@gmail.com>
2025-03-21 14:49:40 -0400

    test

--- train_mcts_flow.py
+++ train_mcts_flow.py
@@ -417,7 +417,7 @@ def main():
                 num_keep,
                 device,
                 sigma=0,
-                n_samples=1000,
+                n_samples=5000,
                 fid=fid,
             )
             print(f"Cycle {cycle + 1} - (branches={num_branches}, keep={num_keep}):")
