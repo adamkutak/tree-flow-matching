39953ad1c7e4369c09b6c3f9bec887c91074d9f5
Adam Stcklov <adamkutak@gmail.com>
2025-03-21 17:10:01 -0400

    test

--- train_mcts_flow.py
+++ train_mcts_flow.py
@@ -363,7 +363,7 @@ def main():
         image_size=image_size,
         channels=channels,
         device=device,
-        num_timesteps=10,
+        num_timesteps=20,
         num_classes=num_classes,
         buffer_size=200,
         load_models=True,
@@ -417,7 +417,7 @@ def main():
                 num_keep,
                 device,
                 sigma=0,
-                n_samples=5000,
+                n_samples=500,
                 fid=fid,
             )
             print(f"Cycle {cycle + 1} - (branches={num_branches}, keep={num_keep}):")
