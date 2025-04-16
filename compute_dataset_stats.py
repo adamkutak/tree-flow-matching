5e98a6752f0100e489b6f0bc785233b3ca756017
Adam Stcklov <adamkutak@gmail.com>
2025-04-16 15:00:42 -0400

    test

--- compute_dataset_stats.py
+++ compute_dataset_stats.py
@@ -56,7 +56,6 @@ def compute_dataset_statistics(dataset_name, feature_dim=64, pca_dim=None):
     transform = transforms.Compose(
         [
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
     )
 
