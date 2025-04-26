0d609d0b516f5509b069382f288d23577c90868c
Adam Stcklov <adamkutak@gmail.com>
2025-04-26 17:31:25 -0400

    test

--- eval_sampler.py
+++ eval_sampler.py
@@ -34,7 +34,7 @@ DEFAULT_DT_STD = 0.7
 DEFAULT_WARP_SCALE = 0.5
 
 # Sampling method defaults
-DEFAULT_SAMPLE_METHOD = "random_search"
+DEFAULT_SAMPLE_METHOD = "path_exploration_timewarp"
 DEFAULT_SCORING_FUNCTION = "dino_score"
 
 # Batch optimization defaults
