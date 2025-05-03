dceda7746a7e9ce2ac521ddffa8e040fddf90627
Adam Stcklov <adamkutak@gmail.com>
2025-05-02 20:09:23 -0400

    test

--- eval_sampler.py
+++ eval_sampler.py
@@ -497,9 +497,10 @@ def generate_and_compute_metrics(
 
         # Generate samples using the specified method
         if sample_method == "regular":
-            # sample = sampler.batch_sample_ode(
-            sample = sampler.batch_sample_ode_divfree(
-                class_label=random_class_labels, batch_size=current_batch_size
+            # sample = sampler.batch_sample_ode_divfree(
+            sample = sampler.batch_sample_ode(
+                class_label=random_class_labels,
+                batch_size=current_batch_size,
             )
         elif sample_method == "path_exploration_timewarp":
             sample = sampler.batch_sample_with_path_exploration_timewarp(
