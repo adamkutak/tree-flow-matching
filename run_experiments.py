5c84ffca05d2b3abbc2a82a265a976f5930d929c
Adam Stcklov <adamkutak@gmail.com>
2025-05-14 14:50:05 -0400

    test

--- run_experiments.py
+++ run_experiments.py
@@ -11,8 +11,7 @@ SAMPLE_METHODS = [
     "random_search",
 ]
 TIMESTEP_CONFIGS = [
-    (20, 0.05, 0.5),
-    (20, 0.05, 0.1),
+    (20, 0.05, 0),
 ]  # (num_timesteps, branch_dt, branch_start_time)
 SAMPLE_SIZES = [128]
 BRANCH_PAIRS = "1:1,2:1,4:1,8:1"  # Always use these branch pairs
@@ -21,7 +20,7 @@ DT_STD = 0.7  # Path exploration time step standard deviation
 WARP_SCALE = 0.5  # Time warp scale factor
 DEVICE = "cuda:3"  # Default device
 NOISE_SCALE = 0.1
-LAMBDA_DIV = 0.25
+LAMBDA_DIV = 0.3
 
 
 def run_experiment(cmd):
