ae985bd7b1ad06676bc6a3c78a9a38c96c626a98
Adam Stcklov <adamkutak@gmail.com>
2025-05-23 20:36:11 -0400

    test

--- diversity_study.py
+++ diversity_study.py
@@ -296,7 +296,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--device",
         type=str,
-        default="cuda:1",
+        default="cuda",
         help="Device to use for computation",
     )
 
