848a575d83e2a5787a85f2c5cdc16d4d7d6c16c9
Adam Stcklov <adamkutak@gmail.com>
2025-06-06 16:13:26 -0400

    test

--- noise_study.py
+++ noise_study.py
@@ -930,19 +930,19 @@ if __name__ == "__main__":
     parser.add_argument(
         "--num_samples",
         type=int,
-        default=1024,
+        default=128,
         help="Number of samples to generate for each experiment",
     )
     parser.add_argument(
         "--batch_size",
         type=int,
-        default=512,
+        default=128,
         help="Batch size for sample generation",
     )
     parser.add_argument(
         "--real_samples",
         type=int,
-        default=10000,
+        default=100,
         help="Number of real samples to use for FID calculation",
     )
 
