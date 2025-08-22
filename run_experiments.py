78c27f139ff36acb3ddc35fdaa77a6466451d1c8
Adam Stcklov <adamkutak@gmail.com>
2025-08-22 18:06:00 -0400

    test

--- run_experiments.py
+++ run_experiments.py
@@ -10,17 +10,17 @@ SAMPLE_METHODS = [
     # "score_sde_path_exploration",
     # "ode_divfree_path_exploration",
     # "random_search_then_divfree_path_exploration",
-    # "random_search",
-    # "noise_search_ode_divfree",
-    # "noise_search_sde",
-    # "random_search_then_noise_search_ode_divfree",
-    # "noise_search_ode_divfree_max",
+    "random_search",
+    "noise_search_ode_divfree",
+    "noise_search_sde",
+    "random_search_then_noise_search_ode_divfree",
+    "noise_search_ode_divfree_max",
     "random_search_then_noise_search_ode_divfree_max",
 ]
 TIMESTEP_CONFIGS = [
     (20, 0.05, 0),
 ]  # (num_timesteps, branch_dt, branch_start_time)
-SAMPLE_SIZES = [128]
+SAMPLE_SIZES = [1024]
 BRANCH_PAIRS = "2:1,4:1,8:1"  # Always use these branch pairs
 SCORING_FUNCTION = "inception_score"  # Default scoring function
 DT_STD = 0.7  # Path exploration time step standard deviation
