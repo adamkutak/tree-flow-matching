c1420f2512ec026abf1ee072e1504aa15e82c0a3
Adam Stcklov <adamkutak@gmail.com>
2025-08-26 10:14:56 -0700

    test

--- run_experiments.py
+++ run_experiments.py
@@ -12,9 +12,9 @@ SAMPLE_METHODS = [
     # "ode_divfree_path_exploration",
     # "random_search_then_divfree_path_exploration",
     # "random_search",
-    "noise_search_ode_divfree",
+    # "noise_search_ode_divfree",
     "noise_search_sde",
-    "random_search_then_noise_search_ode_divfree",
+    # "random_search_then_noise_search_ode_divfree",
     "noise_search_ode_divfree_max",
     "random_search_then_noise_search_ode_divfree_max",
 ]
