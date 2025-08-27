012777011bae9e4402d98561f1893187a5119156
Adam Stcklov <adamkutak@gmail.com>
2025-08-26 21:19:23 -0700

    test

--- run_experiments.py
+++ run_experiments.py
@@ -11,24 +11,24 @@ SAMPLE_METHODS = [
     # "score_sde_path_exploration",
     # "ode_divfree_path_exploration",
     # "random_search_then_divfree_path_exploration",
-    # "random_search",
-    "noise_search_ode_divfree",
-    # "noise_search_sde",
-    "random_search_then_noise_search_ode_divfree",
-    # "noise_search_ode_divfree_max",
-    # "random_search_then_noise_search_ode_divfree_max",
+    "random_search",
+    # "noise_search_ode_divfree",
+    "noise_search_sde",
+    # "random_search_then_noise_search_ode_divfree",
+    "noise_search_ode_divfree_max",
+    "random_search_then_noise_search_ode_divfree_max",
 ]
 TIMESTEP_CONFIGS = [
     (20, 0.05, 0),
 ]  # (num_timesteps, branch_dt, branch_start_time)
-SAMPLE_SIZES = [1024]
+SAMPLE_SIZES = [128]
 BRANCH_PAIRS = "2:1,4:1,8:1"  # Always use these branch pairs
-SCORING_FUNCTION = "inception_score"  # Default scoring function
+SCORING_FUNCTION = "dino_score"  # Default scoring function
 DT_STD = 0.7  # Path exploration time step standard deviation
 WARP_SCALE = 0.5  # Time warp scale factor
 DEVICE = "cuda"  # Default device
 NOISE_SCALE = 0.14
-LAMBDA_DIV = 0.7
+LAMBDA_DIV = 0.9
 
 
 def run_experiment(cmd):
