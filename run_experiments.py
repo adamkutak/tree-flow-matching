2c8c2f15ab853e4423291c8135b79809fe5d36b8
Adam Stcklov <adamkutak@gmail.com>
2025-08-27 12:16:29 -0700

    test

--- run_experiments.py
+++ run_experiments.py
@@ -11,12 +11,12 @@ SAMPLE_METHODS = [
     # "score_sde_path_exploration",
     # "ode_divfree_path_exploration",
     # "random_search_then_divfree_path_exploration",
-    "random_search",
-    # "noise_search_ode_divfree",
-    "noise_search_sde",
-    # "random_search_then_noise_search_ode_divfree",
-    "noise_search_ode_divfree_max",
-    "random_search_then_noise_search_ode_divfree_max",
+    # "random_search",
+    "noise_search_ode_divfree",
+    # "noise_search_sde",
+    "random_search_then_noise_search_ode_divfree",
+    # "noise_search_ode_divfree_max",
+    # "random_search_then_noise_search_ode_divfree_max",
 ]
 TIMESTEP_CONFIGS = [
     (20, 0.05, 0),
@@ -29,7 +29,7 @@ DT_STD = 0.7  # Path exploration time step standard deviation
 WARP_SCALE = 0.5  # Time warp scale factor
 DEVICE = "cuda"  # Default device
 NOISE_SCALE = 0.14
-LAMBDA_DIV = 0.9
+LAMBDA_DIV = 0.6
 
 
 def run_experiment(cmd):
